1bc2c2cab6fadc816140407e44b9899a8422db4a
Aliaksei Smirnou <a.smirnou@vainplex.de>
2024-03-26 15:33:26 +0100

    Add MessageAssembler for handling fragmented messages

--- client.js
+++ client.js
@@ -12,22 +12,28 @@ import { monitorClipboard } from './monitorClipboard.js';
 import { handleMessage } from './messageHandler.js';
 import { sendUpdate } from './sendUpdate.js';
 import { DEFAULT_PORT } from './config.js';
+import MessageAssembler from './fragmenter.js';
+const messageAssembler = new MessageAssembler();
 
 let passphrase = '';
 let client;
 
-function createConnection(hostname, port) {
+function createConnection(hostname, port, disableClipboard = false) {
     client = net.createConnection({ host: hostname, port: port }, () => {
         console.log('Connected to server!');
         const message = 'NETPASTE_HELLO:Netpaste client v1.0';
         const encryptedMessage = encrypt(message, passphrase);
         client.write(encryptedMessage);
-        monitorClipboard(sendUpdate.bind(null, client), passphrase).catch(console.error);
+        if (!disableClipboard) {
+            monitorClipboard(sendUpdate.bind(null, client), passphrase).catch(console.error);
+        }
     });
 
     client.on('data', (data) => {
-        handleMessage(data, passphrase);
-    });
+        messageAssembler.addData(data, (completeMessage) => {
+            handleMessage(completeMessage, passphrase, disableClipboard);
+        });
+});
 
     client.on('end', () => {
         console.log('Disconnected from server');
@@ -44,16 +50,17 @@ function createConnection(hostname, port) {
 function parseCommandLineArgs() {
     const args = process.argv.slice(2);
     const hostname = args[0];
+    const disableClipboard = args.includes('--no-clipboard');
     const port = parseInt(args[1], 10) || DEFAULT_PORT;
     if (!hostname || isNaN(port) || port < 0 || port > 65535) {
         console.error('Invalid command-line arguments. Usage: node client.js <hostname> [port]');
         process.exit(1);
     }
-    return { hostname, port };
+    return { hostname, port, disableClipboard };
 }
 
 getPassphrase((input) => {
     passphrase = input;
-    const { hostname, port } = parseCommandLineArgs();
-    createConnection(hostname, port);
+    const { hostname, port, disableClipboard } = parseCommandLineArgs();
+    createConnection(hostname, port, disableClipboard);
 });
